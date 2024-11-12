23cf8af1fdcd70918e1a75a77c0b389a34a61d30
BernardoVMendonca <bernardo.velloso.mendonca@gmail.com>
2024-11-11 23:33:20 -0300

    SAVE NEW DATA - DONE

--- javascript/template_creator.js
+++ javascript/template_creator.js
@@ -4,8 +4,8 @@ import { copyText, cleanChilds, displayTable, downloadObject, addMenuEventListen
 // ----------------------------------------GLOBAL VARIABLES----------------------------------------
 // ************************************************************************************************
 
-let template = {name: "", text: "", collection: {}};
-let  collection = {}
+let template = { name: "", text: "", collection: {} };
+let collection = {}
 
 // ================================================================================================
 
@@ -15,39 +15,39 @@ let  collection = {}
 // ************************************************************************************************
 
 function generateHeaderTable() {
-    
-   let result = "";
-   let header;
-   for (header in collection.fields){
-       result += `<tr>
+
+    let result = "";
+    let header;
+    for (header in collection.fields) {
+        result += `<tr>
            <td>${collection.fields[header]}</td>
            <td><button  class="copyButton" id="${collection.fields[header]}"')">Copiar Campo</button></td>
        </tr>`
-   }
+    }
 
-   // console.log(result);
-   return result;
+    // console.log(result);
+    return result;
 }
 
-function readCollection(file){
+function readCollection(file) {
 
     template.name = ""
     template.text = ""
     template.collection = {}
     collection = {}
-   const reader = new FileReader();
-   reader.onload = function(e) {
-      try {
-         collection = JSON.parse(e.target.result);
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        try {
+            collection = JSON.parse(e.target.result);
 
-        displayTable("collection-view", ["FIELD NAME", "COPIAR"], generateHeaderTable)
+            displayTable("collection-view", ["FIELD NAME", "COPIAR"], generateHeaderTable)
 
-      } catch (err) {
-         console.error("Erro ao abrir a coleção:", err);
-         alert("Erro ao abrir a coleção.");
-      }
-   };
-   reader.readAsText(file);
+        } catch (err) {
+            console.error("Erro ao abrir a coleção:", err);
+            alert("Erro ao abrir a coleção.");
+        }
+    };
+    reader.readAsText(file);
 
 }
 // ================================================================================================
@@ -58,51 +58,52 @@ function readCollection(file){
 // ************************************************************************************************
 
 // Será utilizado jQuery pois quero usar a GUI disponível na tag <input> para escolha de arquivos :)
-$(document).ready(function() {
-    $("#choose-collection-button").click(function() {
+$(document).ready(function () {
+    $("#choose-collection-button").click(function () {
         $("#collection-selector").click(); // Botão escondido na parte de cima da página hehehehehe
     });
- 
-    $("#collection-selector").change(function(event) {
-       const file = event.target.files[0];
-       readCollection(file)
-       addCopyButtonListener() // O botão de cópia só aparece depois do dados serem lidos
-       cleanChilds("collection-view")
-   });
- });
+
+    $("#collection-selector").change(function (event) {
+        const file = event.target.files[0];
+        readCollection(file)
+        addCopyButtonListener() // O botão de cópia só aparece depois do dados serem lidos
+        cleanChilds("collection-view")
+    });
+});
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
 
-// ************************************************************************************************
-// SAVE TEMPLATE BUTTON EVENT LISTENER
-   const button = document.getElementById('save-template-button');
+    // ************************************************************************************************
+    // SAVE TEMPLATE BUTTON EVENT LISTENER
+    const button = document.getElementById('save-template-button');
 
-   button.addEventListener('click', () => {
-       let templateName = document.getElementById("template-name").value
-       let templateText = document.getElementById("template-area").value
+    button.addEventListener('click', () => {
+        let templateName = document.getElementById("template-name").value
+        let templateText = document.getElementById("template-area").value
 
-       if (templateName.length == 0){
-           alert("O nome do Template precisam estar preenchidas");
-       }
-       else {
-           template.text = templateText
-           template.collection = collection;
+        if (templateName.length == 0) {
+            alert("O nome do Template precisam estar preenchidas");
+        }
+        else {
+            template.text = templateText
+            template.name = templateName;
+            template.collection = collection;
 
-           downloadObject(template, templateName)
-       }
-   })
+            downloadObject(template, template.name)
+        }
+    })
 })
 
 // ************************************************************************************************
 // COPY BUTTON EVENT LISTENER
 
-function addCopyButtonListener(){
-    document.body.addEventListener('click', function(event) {
+function addCopyButtonListener() {
+    document.body.addEventListener('click', function (event) {
         if (event.target.classList.contains("copyButton")) {
-        //    console.log(event.target.id)
-           copyText("$<" + event.target.id + ">$")
+            //    console.log(event.target.id)
+            copyText("$<" + event.target.id + ">$")
         }
     });
 }
--- javascript/template_generator.js
+++ javascript/template_generator.js
@@ -1,11 +1,11 @@
-import { copyText, addPopUpEventListener, cleanChilds, displayTable, addMenuEventListener } from './utils.js';
+import { copyText, addPopUpEventListener, cleanChilds, displayTable, addMenuEventListener, downloadObject } from './utils.js';
 
 
 // ================================================================================================
 // ----------------------------------------GLOBAL VARIABLES----------------------------------------
 // ************************************************************************************************
 
-let template = {name: "", text: "", collection: {}};
+let template = { name: "", text: "", collection: {} };
 let data;
 
 // ================================================================================================
@@ -16,15 +16,15 @@ let data;
 // -------------------------------------------FUNCTIONS--------------------------------------------
 // ************************************************************************************************
 
-function textConstructor(fieldName, fieldValue){
+function textConstructor(fieldName, fieldValue) {
 
    const fieldNameEnabler = document.getElementById("field-name-enabler")
    const fieldValueEnabler = document.getElementById("field-value-enabler")
    const emptyFieldEnabler = document.getElementById("empty-field-enabler")
-   
+
    let newText = ""
 
-   if ((emptyFieldEnabler.checked && fieldValue != "") || !emptyFieldEnabler.checked){
+   if ((emptyFieldEnabler.checked && fieldValue != "") || !emptyFieldEnabler.checked) {
       if (!fieldNameEnabler.checked)
          newText = newText + fieldName + ": "
 
@@ -34,43 +34,43 @@ function textConstructor(fieldName, fieldValue){
    return newText
 }
 
-function searchData(field, value){
+function searchData(field, value) {
    // console.log(field, value)
 
    data = template.collection.data.map(data => {
       // console.log(data[field])
       if (data[field] == value)
          return data
-   }).filter(data => data!==undefined)
+   }).filter(data => data !== undefined)
 
 
 
    let inputList = document.getElementsByClassName("search-value-input")
 
    // console.log(inputList, typeof(inputList))
-   try{
+   try {
       Object.keys(data[0]).forEach(element => {
          Array.from(inputList).map(elementHTML => {
             // console.log(elementHTML.id, "input_"+ element, data[element])
-            if (elementHTML.id == ("input_"+ element))
+            if (elementHTML.id == ("input_" + element))
                document.getElementById(elementHTML.id).value = data[0][element]
             // console.log(elementHTML)
             return elementHTML
-         }).filter(elementHTML => elementHTML!==undefined)
+         }).filter(elementHTML => elementHTML !== undefined)
       });
-   }catch (e) {
+   } catch (e) {
       console.log(e)
    }
 }
 
 function generateHeaderTable() {
-    
+
    // console.log(template.collection)
    let result = "";
    let header;
-   for (header in template.collection.fields){
-      
-       result += `<tr>
+   for (header in template.collection.fields) {
+
+      result += `<tr>
            <td>${template.collection.fields[header]}</td>
            <td><input class="search-value-input" id="input_${template.collection.fields[header]}" placeholder="Value to search" value=""></td>
            <td><button  class="searchButton" id="${template.collection.fields[header]}">Q</button></td>
@@ -80,19 +80,19 @@ function generateHeaderTable() {
    return result;
 }
 
-function readTemplate(file){
+function readTemplate(file) {
 
    template.name = ""
    template.text = ""
    template.collection = {}
-   
+
    const reader = new FileReader();
-   reader.onload = function(e) {
+   reader.onload = function (e) {
       try {
          template = JSON.parse(e.target.result);
 
-      displayTable("template-view", ["Field Name", "VALUE", "SEARCH"], generateHeaderTable)
-   
+         displayTable("template-view", ["Field Name", "VALUE", "SEARCH"], generateHeaderTable)
+
       } catch (err) {
          console.error("Erro ao abrir o template:", err);
          alert("Erro ao abrir a template.");
@@ -109,46 +109,98 @@ function readTemplate(file){
 // ************************************************************************************************
 
 // Será utilizado jQuery pois quero usar a GUI disponível na tag <input> para escolha de arquivos :)
-$(document).ready(function() {
-   $("#choose-template-button").click(function() {
-       $("#template-selector").click(); // Botão escondido na parte de cima da página hehehehehe
+$(document).ready(function () {
+   $("#choose-template-button").click(function () {
+      $("#template-selector").click(); // Botão escondido na parte de cima da página hehehehehe
    });
 
-   $("#template-selector").change(function(event) {
+   $("#template-selector").change(function (event) {
       const file = event.target.files[0];
       readTemplate(file)
       addSearchButtonListener() // o botão de procura só aparece depois dos dadados serem lidos
       cleanChilds("template-view")
-  });
+   });
 });
 
 document.addEventListener('DOMContentLoaded', () => {
 
-// ************************************************************************************************
-// GENERATE BUTTON EVENT LISTENER
+   // ************************************************************************************************
+   // DOWNLOAD UPDATED COLLECTION BUTTON EVENT LISTENER
+   const downloadButton = document.getElementById("download-udpated-collection-button");
+
+   downloadButton.addEventListener('click', () => {
+      downloadObject(template, template.name);
+
+   })
+
+
+
+   // ************************************************************************************************
+   // SAVE NEW DATA BUTTON EVENT LISTENER
+
+   const saveDataButton = document.getElementById("save-data-button");
+
+   saveDataButton.addEventListener('click', () => {
+
+      try {
+
+         const newId = document.getElementById(`input_${template.collection.id}`).value
+
+         const loadedData = template.collection.data.map(object => {
+
+            return object[template.collection.id]
+
+         }).filter(element => element !== undefined)
+
+
+         // console.log(loadedData)
+
+         if (loadedData.indexOf(newId) != -1) {
+            alert("Id já está presente na sua coleção de dados.\nTente com outro ID")
+         } else {
+            const auxObj = {}
+            template.collection.fields.forEach(field => {
+               auxObj[field] = document.getElementById(`input_${field}`).value
+            })
+
+            // console.log(auxObj)
+            template.collection.data.push(auxObj)
+         }
+
+      } catch (e) {
+         alert("ID não encontrado ou não existente.\nVerifique a sua coleção")
+      }
+
+
+
+   })
+
+
+   // ************************************************************************************************
+   // GENERATE BUTTON EVENT LISTENER
 
    const generateButton = document.getElementById("generate-button");
 
    generateButton.addEventListener('click', () => {
 
       let templateTextArea = document.getElementById("template-textarea")
-      
+
       templateTextArea.value = " "
-      try{
-         if (template.text !== undefined){
+      try {
+         if (template.text !== undefined) {
             let aux = template.text
             template.collection.fields.forEach(element => {
-               aux = aux.replace(new RegExp("\\$<" + element + ">\\$", "g"),textConstructor(element, document.getElementById("input_"+element).value))
+               aux = aux.replace(new RegExp("\\$<" + element + ">\\$", "g"), textConstructor(element, document.getElementById("input_" + element).value))
             })
             templateTextArea.value = aux
          }
-      }catch(e){
+      } catch (e) {
          console.log(e)
       }
    })
 
-// ************************************************************************************************
-// COPY BUTTON EVENT LISTENER
+   // ************************************************************************************************
+   // COPY BUTTON EVENT LISTENER
    const copyButton = document.getElementById("copy-button");
 
    copyButton.addEventListener('click', () => {
@@ -159,10 +211,10 @@ document.addEventListener('DOMContentLoaded', () => {
 // ************************************************************************************************
 // SEARCH BUTTON EVENT LISTENER
 function addSearchButtonListener() {
-   document.body.addEventListener('click', function(event) {
-     if (event.target.classList.contains("searchButton"))
-        searchData(event.target.id, document.getElementById("input_"+event.target.id).value)
- });
+   document.body.addEventListener('click', function (event) {
+      if (event.target.classList.contains("searchButton"))
+         searchData(event.target.id, document.getElementById("input_" + event.target.id).value)
+   });
 }
 
 // ************************************************************************************************
